ba9b55f2f9ce4657e59892817f2dbf735a124fed
Zac Wasielewski <zac@wasielewski.org>
2015-10-10 09:32:17 -0400

    Add support for --only and --ignore options

--- jekyll-typeset.rb
+++ jekyll-typeset.rb
@@ -1,5 +1,5 @@
-require 'open3'
 require 'tempfile'
+#require 'open3'
 
 module Jekyll
   class Typeset < Plugin
@@ -8,12 +8,30 @@ module Jekyll
       @site = site
     end
 
+    def options
+      return '' if !@site.config.has_key?('typeset')
+      options = []
+
+      if @site.config['typeset'].has_key?('ignore')
+        options << %(--ignore "#{@site.config['typeset']['ignore']}")
+      end
+      if @site.config['typeset'].has_key?('only')
+        options << %(--only "#{@site.config['typeset']['only']}")
+      end
+
+      options.join(' ')
+    end
+
     def convert(html)
       tmp = Tempfile.new('jekyll-typeset')
       begin
         tmp.write(html)
         tmp.rewind
-        out, err, st = Open3.capture3('typeset-js', tmp.path)
+        shell_cmd = "typeset-js #{self.options}".strip
+        # Open3 doesn't want to cooperate with -- options,
+        # so fall back to the standard backticks instead:
+        #out, err, st = Open3.capture3(shell_cmd, tmp.path)
+        out = `#{shell_cmd} #{tmp.path}`
       ensure
         tmp.close
         tmp.unlink
